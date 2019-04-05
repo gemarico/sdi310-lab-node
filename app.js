7b14fe4816d750a64c10ca6dcd168bdc3d853fba
GemaRico <UO238096@uniovi.es>
2019-04-05 09:33:27 +0200

    SDI-NodeJS-9.7 y 9.8- Manejo de errores en la aplicación y usando Https

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var crypto = require('crypto');
 var gestorBD = require("./modules/gestorBD.js");
 var expressSession = require('express-session');
+var fs = require('fs');
+var https = require('https');
+
 app.use(expressSession({
     secret: 'abcdefg',
     resave: true,
@@ -106,7 +109,19 @@ require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2,
 app.get('/', function (req, res) {
     res.redirect('/tienda');
 });
+
+app.use( function (err, req, res, next ) {
+    console.log("Error producido: " + err); //we log the error in our db
+    if (! res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
+
 // lanzar el servidor
-app.listen(app.get('port'), function() {
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function() {
     console.log("Servidor activo");
 });
