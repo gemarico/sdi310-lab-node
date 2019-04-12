bf1da2ea24dfeda3a22556d2e13dd68f0e5d5885
GemaRico <UO238096@uniovi.es>
2019-04-12 09:58:23 +0200

    SDI-NodeJS-10.5- SW Consumir SW desde NodeJS

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Módulos
 var express = require('express');
 var app = express();
+
+var rest = require('request');
+app.set('rest',rest);
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
